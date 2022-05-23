ce3c2d5fd8aea112dbee80bfac1f183a83096472
Sabrina <sketelone@gmail.com>
2022-05-23 15:23:54 -0700

    add rockpaperscissors function

--- javascript.js
+++ javascript.js
@@ -1,11 +1,35 @@
 
 function computerPlay() {
     if (Math.floor(Math.random()) < 0.33) {
-        return "Rock"
+        return "rock"
     } else if (Math.floor(Math.random()) > 0.67){
-        return "Paper"
+        return "paper"
     } else {
-        return "Scissors"
+        return "scissors"
     }
 }
 
+function rockPaperScissors(playerSelection,computerSelection) {
+    playerSelection = toLowerCase(playerSelection)
+    if (playerSelection == computerSelection) {
+        return "It's a tie!"
+    } else if (playerSelection == "rock") {
+        if (computerSelection == "paper") {
+            return "Computer wins!"
+        } else if (computerSelection == "scissors") {
+            return "Player wins!"
+        }
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "scissors") {
+            return "Computer wins!"
+        } else if (computerSelection == "rock") {
+            return "Player wins!"
+        }
+    } else if (playerSelection == "scissors") {
+        if (computerSelection == "rock") {
+            return "Computer wins!"
+        } else if (computerSelection == "paper") {
+            return "Player wins!"
+        }
+    }
+}
